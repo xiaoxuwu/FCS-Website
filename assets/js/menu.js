54079c6bf8f70c96e05a76862809ed92547d5c86
Carter Wu <carter.wu@ucla.edu>
2016-12-20 14:59:28 -0800

    created other blank files

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,3 +1,16 @@
+// MANIFEST CONSTANTS
+var index = document.URL.search("index") != -1 ? '<li class="active">' : '<li>';
+var enroll = document.URL.search("enroll") != -1 ? '<li class="active">' : '<li>';
+var s_sched = document.URL.search("school_schedule") != -1 ? '<li class="active">' : '<li>';
+var v_sched = document.URL.search("volunteer_schedule") != -1 ? '<li class="active">' : '<li>';
+var classes = document.URL.search("classes") != -1 ? '<li class="active">' : '<li>';
+var about = document.URL.search("about") != -1 ? '<li class="active">' : '<li>';
+var dropdown = document.URL.search("school_schedule") != -1 ||
+               document.URL.search("volunteer_schedule") != -1 ||
+               document.URL.search("classes") != -1
+               ? '<li class="dropdown active">' : '<li class="dropdown">';
+
+// MENU
 document.getElementById("NavMenu").innerHTML =
 '<!-- begin nav -->' +
 '<div class="container-fluid">' +
@@ -8,27 +21,27 @@ document.getElementById("NavMenu").innerHTML =
       '<span class="icon-bar"></span>' +
       '<span class="icon-bar"></span>' +
     '</button>' +
-    '<a class="navbar-brand" href="#">Folsom Chinese School</a>' +
+    '<a class="navbar-brand" href="index.html">Folsom Chinese School</a>' +
   '</div>' +
 
   '<!-- Collect the nav links, forms, and other content for toggling -->' +
   '<div class="collapse navbar-collapse" id="navbar-collapse-1">' +
     '<ul class="nav navbar-nav">' +
-      '<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>' +
-      '<li><a href="#">Enroll</a></li>' +
+      index + '<a href="/index.html">Home <span class="sr-only">(current)</span></a></li>' +
+      enroll + '<a href="/assets/static/enroll.html">Enroll</a></li>' +
     '</ul>' +
 
     '<ul class="nav navbar-nav navbar-right">' +
-      '<li class="dropdown">' +
+      dropdown +
           '<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Schedules and Classes <span class="caret"></span></a>' +
           '<ul class="dropdown-menu">' +
-              '<li><a href="#">School Schedule</a></li>' +
-              '<li><a href="#">Volunteer Schedule</a></li>' +
+              s_sched + '<a href="/assets/static/school_schedule.html">School Schedule</a></li>' +
+              v_sched + '<a href="/assets/static/volunteer_schedule.html">Volunteer Schedule</a></li>' +
               '<li role="separator" class="divider"></li>' +
-              '<li><a href="#">Classrooms and Teachers</a></li>' +
+              classes + '<a href="/assets/static/classes.html">Classrooms and Teachers</a></li>' +
           '</ul>' +
         '</li>' +
-      '<li><a href="#">About</a></li>' +
+      about + '<a href="/assets/static/about.html">About</a></li>' +
     '</ul>' +
   '</div><!-- /.navbar-collapse -->' +
 '</div><!-- /.container-fluid -->';
