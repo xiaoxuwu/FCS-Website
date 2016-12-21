b24bf0668fa92f12c54a675e20e48b5c4536acbc
Danny Yang <dzy4@cornell.edu>
2016-12-20 21:55:01 -0800

    changed offset

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,5 +1,4 @@
 //FOOTER
 document.getElementById("footer").innerHTML =        
-	"<div class='col-md-3'></div>"+
-	"<div class='col-md-3'><a class='footerText' href='assets/static/about.html'>About</a></div>"+
+	"<div class='col-md-3 col-md-offset-3'><a class='footerText' href='assets/static/about.html'>About</a></div>"+
 	"<div class='col-md-3'><a class='footerText' href='mailto:folsomchineseschool@gmail.com'>folsomchineseschool@gmail.com</a></div>";
