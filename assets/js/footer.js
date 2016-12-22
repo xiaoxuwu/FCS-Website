0b7ea23e9301ea19297d1ecd166795418e99490b
Carter Wu <carter.wu@ucla.edu>
2016-12-21 17:31:15 -0800

    updated margins

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,7 +1,7 @@
 //FOOTER
 document.getElementById("footer").innerHTML =
-"<div class='col-md-2 col-md-offset-2'><a class='footerText' href='assets/static/about.html'>About</a></div>" +
-"<div class='col-md-3'><a class='footerText' href='mailto:folsomchineseschool@gmail.com'>folsomchineseschool@gmail.com</a></div>" +
+"<div class='col-md-2 col-md-offset-2'><a class='footerText link' href='assets/static/about.html'>About</a></div>" +
+"<div class='col-md-3'><a class='footerText link' href='mailto:folsomchineseschool@gmail.com'>folsomchineseschool@gmail.com</a></div>" +
 "<div class='col-md-3 footerText'>Copyright © Folsom Chinese School</div>";
 
 // Window load event used just in case window height is dependant upon images
@@ -15,7 +15,7 @@ $(window).bind("load", function() {
 
 	function footerAlign() {
 		footerHeight = $footer.height();
-		footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+		footerTop = ($(window).scrollTop()+$(window).height()-footerHeight) + "px";
 
 		if ( ($(document.body).height()+footerHeight) < $(window).height()) {
 			$footer.css({
@@ -24,12 +24,12 @@ $(window).bind("load", function() {
 			})
 		} else {
 			$footer.css({
-				position: "static"
+				position: "static",
+				top: footerTop
 			})
 		}
 	};
 
 	$(window)
-	.scroll(positionFooter)
-	.resize(positionFooter)
+	.resize(footerAlign)
 });
